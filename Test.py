b33b0bf764141037467a476b63f2f89c61d3c88a
Amisi Jospin Hassan <40860198+AmisiJospin@users.noreply.github.com>
2021-11-15 11:44:35 +0200

    I tried to add a new file just to test a code

---
+++ Test.py
@@ -0,0 +1,6 @@
+def students:
+  Name = "Jospin";
+  Age = 55;
+  Location = "Lilongwe, Area 3, Tsiranana Road";
+
+  print("Your name is " + Name + " and your age "+ Age + " while the locatio  is "+ Location);
